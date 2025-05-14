fb6abda67eba4dc85aa47fb430a71df8c0a705ec
Guilherme Bracero <guibragon@gmail.com>
2025-05-14 18:48:26 -0300

    feat: listagem de produtos

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Carro(models.Model):
+    codigoCarro = models.SmallAutoField(max_length=10, primary_key=True, db_column="CodigoCarro", verbose_name="Código Interno")
+    modelo = models.CharField(max_length=50, db_column="Modelo", verbose_name="Modelo do Carro")
+    marca = models.CharField(max_length=50, db_column="Marca", verbose_name="Marca do Carro")
+    ano = models.IntegerField(db_column="Ano", verbose_name="Ano do Carro")
+    cor = models.CharField(max_length=20, db_column="Cor", verbose_name="Cor do Carro")
+    dataEntrada = models.DateField(db_column="DataEntrada", verbose_name="Data de Entrada")
+    combustivel = models.CharField(choices=(
+        ("G", "Gasolina"),
+        ("E", "Etanol"),
+        ("D", "Diesel"),
+        ("F", "Flex"),
+        ("ET", "Elétrico"),
+        ("H", "Híbrido"),
+    ), max_length=20, db_column="Combustivel", verbose_name="Tipo de Combustível")
+
+    def __str__(self):
+        return f"{self.modelo} - {self.marca} ({self.ano})"
+    
+    class Meta:
+        db_table = "Carro"
+        verbose_name = "Carro"
+        verbose_name_plural = "Carros"
+        ordering = ["modelo"]
--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.http import HttpResponse
+
+from .models import Carro
+
 
 class ListarProdutoView(ListView):
+    template_name = "index.html"
+
+    def get_queryset(self):
+        try:
+            return Carro.objects.all()
+        except Carro.DoesNotExist:
+            return HttpResponse(status=404, content="Carros não encontrados.")
+    
     def get(self, request):
-        return render(request, "index.html")
+        try:
+            carros = self.get_queryset()
+            return render(request, self.template_name, {"carros": carros})
+        except Carro.DoesNotExist:
+            return HttpResponse(status=404, content="Carros não encontrados.")
