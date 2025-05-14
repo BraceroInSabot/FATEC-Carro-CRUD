4169f127d9a68b896fceab023a25ddfda5d931b6
Guilherme Bracero <guibragon@gmail.com>
2025-05-14 19:48:28 -0300

    feat: editar produto

--- produto/urls.py
+++ produto/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListarProdutoView
+from .views import ListarProdutoView, EditarProdutoView
 
 urlpatterns = [
     path("listar/", ListarProdutoView.as_view(), name="listar_produto"),
+    path("editar/<int:pk>", EditarProdutoView.as_view(), name="editar_produto"),
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from django.http import HttpResponse
 
 from .models import Carro
@@ -41,3 +41,46 @@ class ListarProdutoView(ListView):
             return HttpResponse(status=201, content="Carro cadastrado com sucesso.")
         except Exception as e:
             return HttpResponse(status=400, content=f"Erro ao cadastrar carro: {e}")
+        
+class EditarProdutoView(UpdateView):
+    template_name = "editar.html"
+
+    def get_queryset(self, id: int):
+        try:
+            return Carro.objects.get(codigoCarro=id)
+        except Carro.DoesNotExist:
+            return False
+    
+    def get(self, request, *args, **kwargs):
+        codigo = kwargs.get("pk")
+        try:
+            carro = self.get_queryset(codigo)
+            if not carro:
+                return HttpResponse(status=404, content="Carro não encontrado.")
+            return render(request, self.template_name, {"carro": carro})
+        except Carro.DoesNotExist:
+            return HttpResponse(status=404, content="Carro não encontrado.")
+        
+    def post(self, request, *args, **kwargs):
+        codigo = kwargs.get("pk")
+        modelo = request.POST.get("modelo")
+        marca = request.POST.get("marca")
+        ano = request.POST.get("ano")
+        cor = request.POST.get("cor")
+        dataEntrada = request.POST.get("dataEntrada")
+        combustivel = request.POST.get("combustivel")
+        
+        try:
+            carro = self.get_queryset(codigo)
+            if not carro:
+                return HttpResponse(status=404, content="Carro não encontrado.")
+            carro.modelo = modelo
+            carro.marca = marca
+            carro.ano = ano
+            carro.cor = cor
+            carro.dataEntrada = dataEntrada
+            carro.combustivel = combustivel
+            carro.save()
+            return HttpResponse(status=200, content="Carro editado com sucesso.")
+        except Exception as e:
+            return HttpResponse(status=400, content=f"Erro ao editar carro: {e}")
